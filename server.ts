87cec08f4b8aa771ace4fb6097e7997763935e95
angusfng <z5207356@student.unsw.edu.au>
2021-07-03 17:32:07 +1000

    setup for heroku

--- server.ts
+++ server.ts
@@ -1,28 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
 import SpotifyWebApi from "spotify-web-api-node";
 import cors from "cors";
 
 // Setup
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+dotenv.config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Delete later
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
 // Spotify API wrapper
 const spotifyAPI = new SpotifyWebApi({
-  clientId: "4bed51c856c54870906b1fd174911b53",
-  clientSecret: "b6c02dcb075a4aa9926fce20c69062a9",
-  redirectUri: "http://localhost:3000",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
 });
 
-// Refresh the access token
+app.get("/", (req, res) => {
+  res.send("Top Spotify authorization");
+});
+
+// Refresh the access token using refresh token
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   spotifyAPI.setRefreshToken(refreshToken);
@@ -37,12 +38,13 @@ app.post("/refresh", (req, res) => {
       });
     })
     .catch((error) => {
+      console.log("refresh error");
       console.error(error.body.error_description);
       res.sendStatus(400);
     });
 });
 
-// Login to spotify using authorization code
+// Get access token using authorization code
 app.post("/getAccess", (req, res) => {
   const authCode = req.body.authCode;
 
@@ -56,6 +58,7 @@ app.post("/getAccess", (req, res) => {
       });
     })
     .catch((error) => {
+      console.log("access error");
       console.error(error.body.error_description);
       res.sendStatus(400);
     });
